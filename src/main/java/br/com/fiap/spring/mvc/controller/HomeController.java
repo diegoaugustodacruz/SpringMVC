23d0dc18f046661948f53bfb78a983c4b1e138ca
diego.cruz <diego.cruz@accountfy.com>
2022-03-28 18:39:22 -0300

    iniciando projeto

---
+++ src/main/java/br/com/fiap/spring/mvc/controller/HomeController.java
@@ -0,0 +1,14 @@
+package br.com.fiap.spring.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/inicio")
+	public String index() {
+		return "home/index";
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/home/index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>SPRING MVC Hello World</title>
+</head>
+<body>
+
+	<h1>Ola Mundo</h1>
+
+</body>
+</html>
